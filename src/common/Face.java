bbdc591e4553033adc3ba35fb10022f3176df331
JordanPottruff <jordanpottruff@gmail.com>
2021-02-06 20:47:27 -0700

    Add face class definition.

---
+++ src/common/Face.java
@@ -0,0 +1,14 @@
+package common;
+
+public class Face {
+
+    private final Vertex v1;
+    private final Vertex v2;
+    private final Vertex v3;
+
+    public Face(Vertex v1, Vertex v2, Vertex v3) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
+}
