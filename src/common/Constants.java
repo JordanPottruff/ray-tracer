b08a88f5c1cf65c5cfdb2e1288bd4671d6196677
JordanPottruff <jordanpottruff@gmail.com>
2021-02-06 21:03:45 -0700

    Add UVW-related getter methods to the Face class.

---
+++ src/common/Constants.java
@@ -0,0 +1,6 @@
+package common;
+
+public class Constants {
+
+    static final double EPSILON = 0.001;
+}
--- src/common/Face.java
+++ src/common/Face.java
@@ -1,5 +1,7 @@
 package common;
 
+import com.github.jordanpottruff.jgml.Vec3;
+
 public class Face {
 
     private final Vertex v1;
@@ -23,4 +25,46 @@ public class Face {
     public Vertex v3() {
         return this.v3;
     }
+
+    public Vec3 position(double u, double v) {
+        checkUVW(u, v);
+        return lerp(u, v, this.v1.position(), this.v2.position(), this.v3.position());
+    }
+
+    public Vec3 normal(double u, double v) {
+        checkUVW(u, v);
+        return lerp(u, v, this.v1.normal(), this.v2.normal(), this.v3.normal());
+    }
+
+    public Vec3 color(double u, double v) {
+        checkUVW(u, v);
+        return lerp(u, v, this.v1.color(), this.v2.color(), this.v3.color());
+    }
+
+    public double opacity(double u, double v) {
+        checkUVW(u, v);
+        return lerp(u, v, v1.opacity(), v2.opacity(), v3.opacity());
+    }
+
+    public double reflectance(double u, double v) {
+        checkUVW(u, v);
+        return lerp(u, v, v1.reflectance(), v2.reflectance(), v3.reflectance());
+    }
+
+    private Vec3 lerp(double u, double v, Vec3 v1, Vec3 v2, Vec3 v3) {
+        double w = 1 - u - v;
+        return v1.scale(w).add(v2.scale(u)).add(v3.scale(v));
+    }
+
+    private double lerp(double u, double v, double v1, double v2, double v3) {
+        double w = 1 - u - v;
+        return w * v1 + u * v2 + v * v3;
+    }
+
+    private void checkUVW(double u, double v) {
+        if (u + v > 1.0 + Constants.EPSILON) {
+            throw new IllegalArgumentException("UVW-coordinates should not sum to more than 1.");
+        }
+    }
+
 }
