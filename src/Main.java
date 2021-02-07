5eb75b5506db4d41415ac02000c8fa01be96d8b0
JordanPottruff <jordanpottruff@gmail.com>
2021-02-06 20:32:08 -0700

    Add initial project structure.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.github.jordanpottruff.jgml.VecN;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VecN vec = new VecN(new double[]{1.0, 2.0, 3.0});
+        System.out.println(vec);
+    }
+}
