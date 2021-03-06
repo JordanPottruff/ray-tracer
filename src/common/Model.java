6549d9ca6c718a3fe5413adec71035add0b2a261
JordanPottruff <jordanpottruff@gmail.com>
2021-03-05 22:38:31 -0700

    Add rectangular prism models.

--- src/common/Model.java
+++ src/common/Model.java
@@ -37,6 +37,10 @@ public class Model {
         )).collect(Collectors.toSet()));
     }
 
+    public Model translate(double x, double y, double z) {
+        return transform(new Mat4.TransformBuilder().translate(x, y, z).build());
+    }
+
     public static Model createSphere(Vec3 position,  double radius, Vec3 color, double opacity, double reflectance, int n) {
         int nSteps = (n%2==0) ? n : n+1; // n must be an even number.
         double stepSize = 2 * Math.PI / nSteps;
@@ -50,19 +54,19 @@ public class Model {
                 double thetaJP1 = ((j + 1) % nSteps) * stepSize;
 
                 // Vertex and normal (i,j)
-                Vec3 pij = xyz(radius, phiI, thetaJ);
+                Vec3 pij = xyz(radius, phiI, thetaJ).add(position);
                 Vec3 nij = pij.subtract(position).normalize();
                 Vertex vij = new Vertex(pij, nij, color, opacity, reflectance);
                 // Vertex and normal (i+1,j)
-                Vec3 pip1j = xyz(radius, phiIP1, thetaJ);
+                Vec3 pip1j = xyz(radius, phiIP1, thetaJ).add(position);
                 Vec3 nip1j = pip1j.subtract(position).normalize();
                 Vertex vip1j = new Vertex(pip1j, nip1j, color, opacity, reflectance);
                 // Vertex and normal (i, j+1)
-                Vec3 pijp1 = xyz(radius, phiI, thetaJP1);
+                Vec3 pijp1 = xyz(radius, phiI, thetaJP1).add(position);
                 Vec3 nijp1 = pijp1.subtract(position).normalize();
                 Vertex vijp1 = new Vertex(pijp1, nijp1, color, opacity, reflectance);
                 // Vertex and normal (i+1, j+1)
-                Vec3 pip1jp1 = xyz(radius, phiIP1, thetaJP1);
+                Vec3 pip1jp1 = xyz(radius, phiIP1, thetaJP1).add(position);
                 Vec3 nip1jp1 = pip1jp1.subtract(position).normalize();
                 Vertex vip1jp1 = new Vertex(pip1jp1, nip1jp1, color, opacity, reflectance);
 
@@ -78,5 +82,101 @@ public class Model {
         double y = r*Math.sin(theta)*Math.sin(phi);
         double z = r*Math.cos(phi);
         return new Vec3(x, y, z);
+    }
+
+    public static Model createCube(Vec3 position, double width, VertexConfig config) {
+        double half = width / 2;
+        double x = position.x();
+        double y = position.y();
+        double z = position.z();
+        return createRectPrism(x-half, x+half, y-half, y+half, z-half, z+half, config);
+    }
+
+    public static Model createRectPrism(double x1, double x2, double y1, double y2, double z1, double z2, VertexConfig config) {
+        double minX = Math.min(x1, x2);
+        double maxX = Math.max(x1, x2);
+        double minY = Math.min(y1, y2);
+        double maxY = Math.max(y1, y2);
+        double minZ = Math.min(z1, z2);
+        double maxZ = Math.max(z1, z2);
+        Set<Face> faces = new HashSet<>();
+        faces.addAll(createRectX(minX, true, minY, maxY, minZ, maxZ, config));
+        faces.addAll(createRectX(maxX, false, minY, maxY, minZ, maxZ, config));
+        faces.addAll(createRectY(minY, true, minX, maxX, minZ, maxZ, config));
+        faces.addAll(createRectY(maxY, false, minX, maxX, minZ, maxZ, config));
+        faces.addAll(createRectZ(minZ, false, minX, maxX, minY, maxY, config));
+        faces.addAll(createRectZ(maxZ, false, minX, maxX, minY, maxY, config));
+        return new Model(faces);
+    }
+
+    public static Set<Face> createRectX(double x, boolean posNormal, double y1, double y2, double z1, double z2, VertexConfig config) {
+        Vec3 normDir = new Vec3(posNormal ? 1.0 : -1.0, 0, 0);
+        Vertex v1 = config.create(new Vec3(x, y1, z1), normDir);
+        Vertex v2 = config.create(new Vec3(x, y1, z2), normDir);
+        Vertex v3 = config.create(new Vec3(x, y2, z2), normDir);
+        Vertex v4 = config.create(new Vec3(x, y2, z1), normDir);
+
+        Set<Face> faces = new HashSet<>();
+        faces.add(new Face(v1, v2, v3));
+        faces.add(new Face(v3, v4, v1));
+        return faces;
+    }
+
+    public static Set<Face> createRectY(double y, boolean posNormal, double x1, double x2, double z1, double z2, VertexConfig config) {
+        Vec3 normDir = new Vec3(0, posNormal ? 1.0 : -1.0, 0);
+        Vertex v1 = config.create(new Vec3(x1, y, z1), normDir);
+        Vertex v2 = config.create(new Vec3(x1, y, z2), normDir);
+        Vertex v3 = config.create(new Vec3(x2, y, z2), normDir);
+        Vertex v4 = config.create(new Vec3(x2, y, z1), normDir);
+
+        Set<Face> faces = new HashSet<>();
+        faces.add(new Face(v1, v2, v3));
+        faces.add(new Face(v3, v4, v1));
+        return faces;
+    }
+
+    public static Set<Face> createRectZ(double z, boolean posNormal, double x1, double x2, double y1, double y2, VertexConfig config) {
+        Vec3 normDir = new Vec3(0, 0, posNormal ? 1.0 : -1.0);
+        Vertex v1 = config.create(new Vec3(x1, y1, z), normDir);
+        Vertex v2 = config.create(new Vec3(x1, y2, z), normDir);
+        Vertex v3 = config.create(new Vec3(x2, y2, z), normDir);
+        Vertex v4 = config.create(new Vec3(x2, y1, z), normDir);
+
+        Set<Face> faces = new HashSet<>();
+        faces.add(new Face(v1, v2, v3));
+        faces.add(new Face(v3, v4, v1));
+        return faces;
+    }
+
+    public static class VertexConfig {
+        private final Vec3 color;
+        private final double opacity;
+        private final double reflectance;
+
+        public VertexConfig(Vec3 color) {
+            this(color, 1.0, 0.0);
+        }
+
+        public VertexConfig(Vec3 color, double opacity, double reflectance) {
+            this.color = color;
+            this.opacity = opacity;
+            this.reflectance = reflectance;
+        }
+
+        public Vec3 color() {
+            return color;
+        }
+
+        public double opacity() {
+            return opacity;
+        }
+
+        public double reflectance() {
+            return reflectance;
+        }
+
+        public Vertex create(Vec3 position, Vec3 normal) {
+            return new Vertex(position, normal, color, opacity, reflectance);
+        }
     }
 }
